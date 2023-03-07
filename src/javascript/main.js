235f7647300f030fc7dca31668e63354a7cf1eac
Anderson Silva <anderson.silvas@hotmail.com>
2023-03-07 11:59:10 -0300

    Criando função para validar CPF

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -56,4 +56,27 @@ segundoBotaoVoltar.addEventListener('click', function(event){
     atual -= 1
 });
 
+// Validação dos campos
 
+function validarCPF (cpf) {
+    cpf = cpf.replace(/[^\d]+/g,''); //remove os caracteres não númericos
+    if (cpf == '') return false; //verifica se o campo cpf está vazio
+
+    if(cpf.length != 11 || /^(\d)\1+$/.test(cpf)) return false;
+
+    var soma = 0
+    var resto;
+
+    for(var i=1; i<=9; i++) soma = soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
+    resto = (soma * 10) % 11;
+    if ((resto == 10) || (resto == 11)) resto = 0;
+    if (resto != parseInt(cpf.substring(9, 10)) ) return false;
+    soma = 0;
+
+    for (var i = 1; i <=10; i++) soma = soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
+    resto = (soma * 10) % 11;
+    if ((resto == 10) || (resto == 11)) resto = 0;
+    if (resto != parseInt(cpf.substring(10, 11) ) ) return false;
+
+    return true;
+}
